522711e558cc29c3cfef25285a8640fdbf622fc3
n3ndor <n3ndor@gmail.com>
2023-02-12 15:43:04 -0300

    added the missing functions from assignment

--- store_products/store_products.py
+++ store_products/store_products.py
@@ -4,17 +4,22 @@ class Store:
         self.list = []
 
     def add_product(self, new_product):
-        self.new_product = list.append(new_product)
-
-    def sell_product(self, id):
-        list[id].pop()
+        self.list.append(new_product)
         return self
 
+    def sell_product(self, id):
+        self.list[id].pop()
+        return self
+
+
     def inflation(self, percent_increase):
-        
+        self.product.price *= self.percent_increase
+        return self
 
     def set_clearance(self, category, percent_discount):
-        pass
+        for i in self.list:
+            self.product.price *= self.percent_discount
+            return self
 
 
 class Product:
@@ -22,18 +27,36 @@ class Product:
         self.name = name
         self.price = price
         self.category = category
+        self.is_increased = True | False
+        self.percent_change = 1.1 # 10%
 
     def update_price(self, percent_change, is_increased):
-        if is_increased == True:
-            self.price = self.price * (1 + self.percent_change)
+        if self.is_increased == True:
+            self.price = self.price * self.percent_change
         else:
-            self.price = self.price / (1 + self.percent_change)
-        return self.price
+            self.price = self.price * self.percent_change
+        return self
 
     def print_info(self):
-        print(self.name)
-        print(self.category)
-        print(self.price)
+        print(f"Product Name: {self.name}, Category: {self.category}, Price: ${self.price}")
+        
         return self
 
 
+kiwi = Product("Kiwi", 20, "Fruit")
+potato = Product("Potato", 5, "Vegetable")
+
+kiwi.print_info()
+kiwi.update_price(50, True).print_info()
+
+potato.print_info()
+potato.update_price(20, False).print_info()
+
+a = Store("Fruit", "list_of_fruits")
+b = Store("Vegetable", "list_of_vegetables")
+
+a.add_product("kiwi")
+b.add_product("potato")
+
+print(a.name, a.list)
+print(b.name, b.list)
